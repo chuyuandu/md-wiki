999b61eda5410ca657ed0a72ae13d518b1d39d8b
cdu <chuyuandu@163.com>
2020-06-15 07:52:54 +0800

    添加build文件夹，支持gitee pages

---
+++ build/precache-manifest.74ff5b0044f4dca853e1853b5818e828.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6340bdd06b3b463024757dd911e1554",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5a52276b833daf9ef1cd",
+    "url": "./static/css/2.6111d483.chunk.css"
+  },
+  {
+    "revision": "02e89cdc02457eef030c",
+    "url": "./static/css/main.7f9aa1e3.chunk.css"
+  },
+  {
+    "revision": "5a52276b833daf9ef1cd",
+    "url": "./static/js/2.8dd18568.chunk.js"
+  },
+  {
+    "revision": "334e2efdf33088266e96c18d64be6822",
+    "url": "./static/js/2.8dd18568.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02e89cdc02457eef030c",
+    "url": "./static/js/main.ccfc3735.chunk.js"
+  },
+  {
+    "revision": "abdafc75a9becfaa40c0",
+    "url": "./static/js/runtime-main.bac915e3.js"
+  },
+  {
+    "revision": "87519cac9cfee04e1d964e2571ee0553",
+    "url": "./static/media/gitee.87519cac.svg"
+  }
+]);
