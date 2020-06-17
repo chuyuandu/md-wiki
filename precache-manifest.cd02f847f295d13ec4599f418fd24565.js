12efacac1dcf8eb4899b51ce91683578c4ecfaaf
chuyuandu <chuyuandu@users.noreply.github.com>
2020-06-17 14:45:23 +0000

    Automated deployment: Wed Jun 17 14:45:23 UTC 2020 8ce3be56a7a942988d7dedee34a5cb9f98860523

---
+++ precache-manifest.cd02f847f295d13ec4599f418fd24565.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec40c893f34b52537aed19777a243c5e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c8c201c72f6061dd6661",
+    "url": "./static/css/2.5007f822.chunk.css"
+  },
+  {
+    "revision": "11d0a22766a314d52adc",
+    "url": "./static/css/main.7f9aa1e3.chunk.css"
+  },
+  {
+    "revision": "c8c201c72f6061dd6661",
+    "url": "./static/js/2.6eada5a7.chunk.js"
+  },
+  {
+    "revision": "334e2efdf33088266e96c18d64be6822",
+    "url": "./static/js/2.6eada5a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11d0a22766a314d52adc",
+    "url": "./static/js/main.d63c7385.chunk.js"
+  },
+  {
+    "revision": "abdafc75a9becfaa40c0",
+    "url": "./static/js/runtime-main.bac915e3.js"
+  },
+  {
+    "revision": "87519cac9cfee04e1d964e2571ee0553",
+    "url": "./static/media/gitee.87519cac.svg"
+  }
+]);
